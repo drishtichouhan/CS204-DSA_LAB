2294fd71247607ad8032824cad4c8f3d7755ffdd
drishtichouhan <45739592+drishtichouhan@users.noreply.github.com>
2019-08-04 13:35:56 +0530

    Add files via upload

---
+++ sub.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+  
+
+  
+ 
+string subtraction(string str1, string str2) 
+{ 
+
+    
+    if (str1.length()<str2.length()) 
+        swap(str1, str2); 
+  
+    
+    string str = ""; 
+  
+     
+    int n1 = str1.length(), n2 = str2.length(); 
+  
+
+    reverse(str1.begin(), str1.end()); 
+    reverse(str2.begin(), str2.end()); 
+      
+    int carry = 0; 
+  
+    
+    
+    for (int i=0; i<n2; i++) 
+    { 
+        
+        int sub = ((str1[i]-'0')-(str2[i]-'0')-carry); 
+          
+         
+        if (sub < 0) 
+        { 
+            sub = sub + 10; 
+            carry = 1; 
+        } 
+        else
+            carry = 0; 
+  
+        str.push_back(sub + '0'); 
+    } 
+  
+    
+    for (int i=n2; i<n1; i++) 
+    { 
+        int sub = ((str1[i]-'0') - carry); 
+          
+         
+        if (sub < 0) 
+        { 
+            sub = sub + 10; 
+            carry = 1; 
+        } 
+        else
+            carry = 0; 
+              
+        str.push_back(sub + '0'); 
+    } 
+  
+    
+    reverse(str.begin(), str.end()); 
+  
+    return str; 
+} 
+  
+ 
+int main() 
+{ 
+    string num1,num2;
+    
+    cin>>num1;
+    
+    cin>>num2;
+    
+    cout << subtraction(num1, num2) << endl; 
+      
+    
+    
+      
+    return 0; 
+} 
+
