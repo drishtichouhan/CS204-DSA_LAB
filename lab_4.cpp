cc64dc0cb0e20d57cdbe1501364cff6d4a985975
drishtichouhan <45739592+drishtichouhan@users.noreply.github.com>
2019-08-21 22:58:30 +0530

    Update lab_4.cpp

--- lab_4.cpp
+++ lab_4.cpp
@@ -1,192 +1,241 @@
 #include <bits/stdc++.h>
-#define longI long long int
-#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-
-
+#include <stack>
+#include <string>
+#include <math.h>
+#include <vector>
 using namespace std;
 
-vector<string> Convert_string_to_vector(string s)
-{
-    int k = s.length();
-    vector<string> str;
-    for(int i = 0; i<k;i++)
-    {
-        string s1="";
-        if((s[i] >= '0' && s[i] <= '9'))
-        {
-            int j = i;
-            while(i!=s.size() && s[i] >= '0' && s[i] <= '9')
-            {
-                s1+=s[i];
-                i++;
-            }
-
-            i--;
-        }
-        else
-        {
-            s1+=s[i];
-        }
-        str.push_back(s1);
-
-    }
-
-    return str;
-}
-int Precedence(string arr)
-{
-    if(arr == "^")
-    return 3;
-    else if(arr == "*" || arr == "/")
-    return 2;
-    else if(arr == "+" || arr == "-")
-    return 1;
-    else
-    return -1;
-}
-vector<string> INFIX_to_POSTFIX(vector<string> temp)
-{
-    stack<string> array;
-    array.push("N");
-    int l = temp.size();
-    vector<string> vec;
-    for(int i = 0; i < l; i++)
-    {
-        if((temp[i][0] >= '0' && temp[i][0] <= '9'))
-            vec.push_back(temp[i]);
-        else if(temp[i] == "(")
-           array.push("(");
-        else if(temp[i] == ")")
-        {
-            while(array.top() != "N" && array.top() != "(")
-            {
-                string c = array.top();
-                array.pop();
-                vec.push_back(c);
-            }
-            if(array.top() == "(")
-            {
-                string c = array.top();
-                array.pop();
-            }
-        }
-        else{
-            while(array.top() != "N" && Precedence(temp[i]) <= Precedence(array.top()))
-            {
-              if(temp[i]!="^" && array.top()!="^"){
-                string c = array.top();
-                array.pop();
-                vec.push_back(c);
-            }
-            else{
-              break;
-            }
-          }
-            if(temp[i] == "^" && array.top()=="^"){
-            array.push(temp[i]);}
-            else{
-              array.push(temp[i]);
-            }
-        }
-
-    }
-    while(array.top() != "N")
-    {
-        string c = array.top();
-        array.pop();
-        vec.push_back(c);
-    }
-
-    return vec;
-
-}
 
 struct node
 {
   string s;
-  node* LEFT=NULL;
-  node* RIGHT=NULL;
+  struct node* LEFT;
+  struct node* RIGHT;
 };
-longI EVALUATION(node *root)
+
+struct node* create(string array)
 {
-   string s=root->s;
-   longI result=0;
-   if(s!="+" && s!="-" && s!="*" && s!="/" && s!="^")
-   return stoi(s);
-   else
-   {
-     int left= EVALUATION(root->LEFT);
-     int right=EVALUATION(root->RIGHT);
-     if(s=="+")
-       result=left+right;
-     else if(s=="-")
-       result=left-right;
-     else if(s=="*")
-       result=left*right;
-     else if(s=="/")
-       result=left/right;
-     else
-     {
-       result=1;
-       for(longI i =0;i<right;i++)result*=left;
-     }
-   }
-   return result;
-}
-node* TREE_CONSTRUCT(vector <string> postfix)
-{
-   node *root=NULL;
-   vector <node*> yolo;
-   int i=0;
-   while(i!=postfix.size())
-   {
-     if(postfix[i]!="+" && postfix[i]!="-" && postfix[i]!="*" && postfix[i]!="/" && postfix[i]!="^")
-       {
-         node *temp=(node *)malloc(sizeof(node));
-         temp->s=postfix[i];
-         for(longI j=0;j<postfix[i].size();j++)
-         if(postfix[i][j]<'0' || postfix[i][j]>'9')return NULL;
-         yolo.push_back(temp);
-       }
-     else
-     {
-       if(yolo.size()<2)return NULL;
-       node *s1=yolo.back();
-       yolo.pop_back();
-       node *s2=yolo.back();
-       yolo.pop_back();
-       node *temp=(node *)malloc(sizeof(node));
-       temp->s=postfix[i];
-       temp->LEFT=s2;
-       temp->RIGHT=s1;
-       yolo.push_back(temp);
-     }
-     i++;
-   }
-   if(yolo.size()!=1)return NULL;
-   return yolo.back();
+  struct node* tmp= (struct node*)malloc(sizeof(struct node));
+  tmp->LEFT = NULL;
+  tmp->RIGHT = NULL;
+  tmp->s = array;
+  return tmp; 
 }
 
+int Precedence(char a)
+{
+  if(a =='(')
+  {
+    return -1;
+  }
+  else if(a=='^')
+  {
+    return 3;
+  }
+  else if(a=='*' || a=='/')
+  {
+    return 2;
+  }
+  else if(a=='+' || a=='-')
+  {
+    return 1;
+  }
+}
+struct node* TREE_CONSTRUCT(string expr)
+{
+  stack<char> temp;
+  stack<struct node* > arr;
+  for(int i=0;i<expr.length();i++)
+  {
+    if(expr[i]>='0' && expr[i]<='9')
+    {
+      int j=i;
+      int k=0;
+      while(expr[j]>='0' && expr[j]<='9')
+      {
+        k = k*10 + (expr[j] - '0');
+        j++;
+      }
+      string tmp1 = to_string(k);
+      
+      arr.push(create(tmp1));
+      i = j-1;
+    }
+    else if(expr[i] == '(')
+    {
+      temp.push(expr[i]);
+
+    }
+    else if(expr[i]=='*' || expr[i] == '-' || expr[i]=='+' || expr[i]=='^' || expr[i]=='/')
+    {
+      if(temp.empty() == 1)
+      {
+        
+        temp.push(expr[i]);
+      }
+      else{
+        if(expr[i]=='^' && temp.top()=='^')
+        {
+          temp.push(expr[i]);
+        }
+          else if(Precedence(temp.top()) < Precedence(expr[i]))
+          {
+            
+           temp.push(expr[i]);
+
+          }
+        else{
+          while(temp.empty() == 0)
+          {
+           if(Precedence(temp.top()) >= Precedence(expr[i]))
+           {
+            string a=" ";
+            a[0] = temp.top();
+            temp.pop();
+            struct node* n = create(a);
+            n->RIGHT = arr.top();
+            arr.pop();
+            struct node* min = create("0");
+            
+            if(arr.empty()==1)
+            {
+              n->LEFT = min;
+            }
+            else
+            {
+              n->LEFT = arr.top();
+              arr.pop();
+            }
+            arr.push(n);
+          }
+          }
+          temp.push(expr[i]);
+        }
+      }
+    }
+
+    
+    else if(expr[i]==')')
+    {
+      
+      while(temp.top() !='(')
+      {
+          string a=" ";
+          a[0] = temp.top();
+          temp.pop();
+          struct node* n = create(a);
+          n->RIGHT = arr.top();
+          arr.pop();
+          struct node* min = create("0");
+          
+          if(arr.empty()==1)
+          {
+              n->LEFT = min;
+          }
+          else
+          {
+            n->LEFT = arr.top();
+            arr.pop();
+          }
+          arr.push(n);
+      }
+      
+      temp.pop();
+    }
+    
+  
+  }
+    while(temp.empty() != 1)
+    {
+      string a=" ";
+      a[0] = temp.top();
+      
+      temp.pop();
+      struct node* n = create(a);
+      n->RIGHT = arr.top();
+      arr.pop();
+      struct node* min = create("0");
+      if(arr.empty()==1)
+      {
+        n->LEFT = min;
+      }
+      else
+      {
+        n->LEFT = arr.top();
+        arr.pop();
+      }
+      arr.push(n);
+    }
+  
+  return arr.top();
+
+}
+
+int EVALUATE(struct node *head)
+{
+  if(head==NULL)
+  {
+    return -1;
+  }
+  if(head->LEFT == NULL && head->RIGHT == NULL)
+  {
+    
+    return stoi(head->s);
+  }
+  
+  int left  = EVALUATE(head->LEFT);
+  int right = EVALUATE(head->RIGHT);
+  
+  string str = head->s;
+  if(str[0] == '+')
+  {
+    
+    return left + right;
+  }
+  if(str[0] == '*'){
+    
+    return left *  right;
+  }
+  if(str[0] == '-')
+  {
+    
+    return left - right;
+  }
+  if(str[0] == '/')
+  {
+      
+    return left / right;
+  }
+  if(str[0] == '^')
+  {
+      
+    return pow(left,right);
+  }
+return -1;
+}
 int main()
 {
-  fastio;
-  int t;
-  cin>>t;
-  while(t--)
+  int test ;
+  cin>>test ;
+  vector< vector<int> > exp(test);
+  for(int i=0;i<test;i++)
   {
     int n;
     cin>>n;
-    for(longI i=0;i<n;i++)
+    for(int j=0;j<n;j++)
     {
-      string s;
-      cin>>s;
-      vector <string> converted=Convert_string_to_vector(s);
-      converted=INFIX_to_POSTFIX(converted);
-      node* root=TREE_CONSTRUCT(converted);
-      if(root!=NULL)
-      cout<<EVALUATION(root)<<"\n";
-      else cout<<"CANT BE EVALUATED\n";
+      string str;
+      cin>>str;
+      exp[i].push_back(EVALUATE(TREE_CONSTRUCT(str)));
     }
   }
+  for(int k=0;k<test;k++)
+  {
+    for(int j=0;j<exp[k].size();j++)
+    {
+      cout<<exp[k][j]<<endl;
+    }
+  }
+  
   return 0;
 }
