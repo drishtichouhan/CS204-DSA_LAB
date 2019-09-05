0e12cf9f799e5adc5211c80848b0d0452c595cd6
drishtichouhan <45739592+drishtichouhan@users.noreply.github.com>
2019-09-05 08:38:06 +0530

    Update lab_6.cpp

--- lab_6.cpp
+++ lab_6.cpp
@@ -1,82 +1,121 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-struct node
-{
-  long long int user_id;
-  long long int recharge;
-  struct node* left ;
-  struct node* right;
-};
-struct node* create_node(long long int ID,long long int recharge)
-{
-  struct node* NODE = new struct node;
-  NODE->user_id = ID;
-  NODE->recharge = recharge;
-  NODE->left = NULL;
-  NODE->right = NULL;
-  return node;
+int Search(vector<pair<int,int>> &A, int l, int r, int a) 
+{ 
+    if (r >= l) 
+    { 
+        int middle;
+        middle=l + (r - l) / 2; 
+        if (A[middle].first == a) return middle; 
+        if (A[middle].first > a) return Search(A, l, middle - 1, a); 
+  
+        return Search(A, middle + 1, r, a); 
+    } 
+  
+    return -1; 
 }
-struct node* insert(struct node* root,long long int UID,long long money)
-{
-  if(root==NULL)
-  {
-    return create_node(UID,recharge)
 
-  }
-  if(UID<root->user_id)
-  {
-    root->left = insert(root->left,UID);
-  }
-   if(UID>root->user_id)
-  {
-    root->right = insert(root->right,UID);
-  }
-  if(UID==root->user_id)
-  {
-    root->recharge+=money;
-  }
-}
-struct node* search(struct node* root, long long int ID)
-{
-  struct node* tmp;
-  tmp = root;
-
-  while(root!=NULL)
-  {
-    if(UID>root->user_id)
-    {
-      root=root->right;
-      search(root,ID);
-
-    }
-    if(UID<root->user_id)
-    {
-      root=root->left;
-      search(root,ID);
-    }
-
-
-    else return root;
-  }
-  return tmp;
-
-}
 int main()
 {
-  int users,q;
-  cin>>users;
-  cin>>q;
-  while(q)
-  {
-    int num;
-    cin>>num;
-    if(num==1)
+    long long int n,q;
+    cin>>n>>q;
+    long long int max_ID=-1;
+    long long int max_recharge=INT_MIN;
+    vector<pair<int,int>> queries;
+    for(int i=0;i<q;i++)
     {
-      int a,b;
-      cin>>a>>b;
-      search()
+        int num;
+        cin>>num;
+        if(num==1)
+        {
+            int user_id,recharge;
+            cin>>user_id>>recharge;
+            pair <int,int> node;
+            node.first=user_id;
+            node.second=recharge;
+            
+            queries.push_back(node);
+        }
+        else if(num==2)
+        {
+            
+            pair <int,int> node;
+            node.first=-1;
+            node.second=0;
+            
+             queries.push_back(node);
+        }
+        else
+        {
+            pair <int,int> node;
+            node.first=-2;
+            node.second=0;
+            
+            queries.push_back(node);
+
+        }
+
     }
-  }
+    vector<int> vec;
+    for(int i=0;i<q;i++)
+    {
+        if(queries[i].first>0)
+        {
+            vec.push_back(queries[i].first);
+        }
+    }
+    vector <pair < int,int > > USER_ID;
+    sort(vec.begin(),vec.end());
+    int y=0;
+    for(int i=0;i<vec.size();i++)
+    {
+        if(vec[i]!=y)
+        {
+             pair<int,int> p;
+            p.first=vec[i];
+            p.second=0;
+            USER_ID.push_back(p);
+        }
+        y=vec[i];
+    }
+
+    for(int i=0;i<q;i++)
+    {
+       
+        if(queries[i].first>0)
+        {
+            int s;
+            s= queries[i].first;
+            int x;
+            x=Search(USER_ID, 0, (USER_ID.size()-1),s);
+            
+            USER_ID[x].second=USER_ID[x].second+queries[i].second;
+            if(USER_ID[x].second>max_recharge)
+            {
+                max_recharge=USER_ID[x].second;
+                max_ID=USER_ID[x].first;    
+            }
+        }
+        else if(queries[i].first==-1)
+        {
+            if(max_ID==-1)
+            {
+                cout<<"No data available.";
+            }
+            else
+            {
+                cout<<"\n\n"<<max_ID;
+            }
+            cout<<endl;
+
+        }
+        else
+        {
+            cout<<"Invalid Input"<<endl;
+        }
+        
+    }
+
+
 
 }
