e6922cf48553a8e3839d9c910c3a3853fa83902a
drishtichouhan <45739592+drishtichouhan@users.noreply.github.com>
2019-11-04 13:52:05 +0530

    Update Binary_tree.cpp

--- lab_assignment_slot1/Binary_tree.cpp
+++ lab_assignment_slot1/Binary_tree.cpp
@@ -1,91 +1,199 @@
-//Question : Find mirror of a given node in Binary Tree
-
-// Code :
-
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdlib>
 using namespace std;
-struct Node{
-	int key;
-	struct Node *left;
-	struct Node *right;
+struct tree {
+    int info;
+    tree *Left, *Right;
 };
-struct Node* new_node(int k)
+tree* root;
+class Binary_tree {
+public:
+    Binary_tree();
+    void insert1(int);
+    tree* insert2(tree*, tree*);
+    void Delete(int);
+    void pretrav(tree*);
+    void intrav(tree*);
+    void posttrav(tree*);
+};
+Binary_tree::Binary_tree()
 {
-	struct Node* n = (struct Node*)malloc(sizeof(struct Node*));
-	if(n!=NULL)
-	{
-		n->key=k;
-		n->left=NULL;
-		n->right=NULL;
-		return n;
-	}
-	else 
-	{
-		cout<<"Memory allocation failed";
-
-	}
+    root = NULL;
 }
-
-int Find_mirror(int val,struct Node* left , struct Node* right)
+tree* Binary_tree::insert2(tree* temp, tree* newnode)
 {
-	if(right==NULL||left==NULL)return 0;
-	if(left->key==val)return right->key;
-	if(right->key==val)return left->key;
-	int value = Find_mirror(val,left->left,right->right);
-	if(value)return value;
-	Find_mirror(val,left->right,right->left);
+    if (temp == NULL) {
+        temp = newnode;
+    }
+    else if (temp->info < newnode->info) {
+        insert2(temp->Right, newnode);
+        if (temp->Right == NULL)
+            temp->Right = newnode;
+    }
+    else {
+        insert2(temp->Left, newnode);
+        if (temp->Left == NULL)
+            temp->Left = newnode;
+    }
+    return temp;
 }
-
-int MIRROR(struct Node* root, int val)
+void Binary_tree::insert1(int n)
 {
-	if(root==NULL)return 0;
-	if(val==root->key)return val;
-	return Find_mirror(val,root->left,root->right);
+    tree *temp = root, *newnode;
+    newnode = new tree;
+    newnode->Left = NULL;
+    newnode->Right = NULL;
+    newnode->info = n;
+    root = insert2(temp, newnode);
 }
-void insert(struct Node* temp, int key) 
-{ 
-    queue<struct Node*> q; 
-    q.push(temp);
-    while (!q.empty()) { 
-        struct Node* temp = q.front(); 
-        q.pop(); 
-  
-        if (!temp->left) { 
-            temp->left = new_node(key); 
-            break; 
-        } else
-            q.push(temp->left); 
-  
-        if (!temp->right) { 
-            temp->right = new_node(key); 
-            break; 
-        } else
-            q.push(temp->right); 
-    } 
-} 
-  
-
+void Binary_tree::pretrav(tree* t = root)
+{
+    if (root == NULL) {
+        cout << "Nothing to display";
+    }
+    else if (t != NULL) {
+        cout << t->info << " ";
+        pretrav(t->Left);
+        pretrav(t->Right);
+    }
+}
+void Binary_tree::intrav(tree* t = root)
+{
+    if (root == NULL) {
+        cout << "Nothing to display";
+    }
+    else if (t != NULL) {
+        intrav(t->Left);
+        cout << t->info << " ";
+        intrav(t->Right);
+    }
+}
+void Binary_tree::posttrav(tree* t = root)
+{
+    if (root == NULL) {
+        cout << "Nothing to display";
+    }
+    else if (t != NULL) {
+        posttrav(t->Left);
+        posttrav(t->Right);
+        cout << t->info << " ";
+    }
+}
+void Binary_tree::Delete(int key)
+{
+    tree *temp = root, *parent = root, *marker;
+    if (temp == NULL)
+        cout << "The tree is empty" << endl;
+    else {
+        while (temp != NULL && temp->info != key) {
+            parent = temp;
+            if (temp->info < key) {
+                temp = temp->Right;
+            }
+            else {
+                temp = temp->Left;
+            }
+        }
+    }
+    marker = temp;
+    if (temp == NULL)
+        cout << "No node present";
+    else if (temp == root) {
+        if (temp->Right == NULL && temp->Left == NULL) {
+            root = NULL;
+        }
+        else if (temp->Left == NULL) {
+            root = temp->Right;
+        }
+        else if (temp->Right == NULL) {
+            root = temp->Left;
+        }
+        else {
+            tree* temp1;
+            temp1 = temp->Right;
+            while (temp1->Left != NULL) {
+                temp = temp1;
+                temp1 = temp1->Left;
+            }
+            if (temp1 != temp->Right) {
+                temp->Left = temp1->Right;
+                temp1->Right = root->Right;
+            }
+            temp1->Left = root->Left;
+            root = temp1;
+        }
+    }
+    else {
+        if (temp->Right == NULL && temp->Left == NULL) {
+            if (parent->Right == temp)
+                parent->Right = NULL;
+            else
+                parent->Left = NULL;
+        }
+        else if (temp->Left == NULL) {
+            if (parent->Right == temp)
+                parent->Right = temp->Right;
+            else
+                parent->Left = temp->Right;
+        }
+        else if (temp->Right == NULL) {
+            if (parent->Right == temp)
+                parent->Right = temp->Left;
+            else
+                parent->Left = temp->Left;
+        }
+        else {
+            tree* temp1;
+            parent = temp;
+            temp1 = temp->Right;
+            while (temp1->Left != NULL) {
+                parent = temp1;
+                temp1 = temp1->Left;
+            }
+            if (temp1 != temp->Right) {
+                temp->Left = temp1->Right;
+                temp1->Right = parent->Right;
+            }
+            temp1->Left = parent->Left;
+            parent = temp1;
+        }
+    }
+    delete marker;
+}
 int main()
 {
-	cout<<"Enter the number of elements in the tree:"<<"\n";
-	int num;
-	cin>>num;
-	int arr[1000];
-	struct Node* root=NULL;
-	
-	for(int i=0;i<num;i++)cin>>arr[i];
-	for(int i=0;i<num;i++)
-	{		cout<<"YO";
-		insert(root,arr[i]); 
-	}
-	int target;
-	cin>>target;
-	int mirror_value=MIRROR(root,target);
-	if (mirror_value)  
-        cout << "Mirror of Node " << target  
-             << " is Node " << mirror_value << endl;  
-    else
-        cout << "Mirror of Node " << target  
-             << " is NULL! " << endl; 
-
+    Binary_tree bt;
+    int choice, n, key;
+    while (1) {
+        cout << "\n\t1. Insert\n\t2. Delete\n\t3. Preorder Traversal\n\t4. Inorder Treversal\n\t5. Postorder Traversal\n\t6. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            cout << "Enter item: ";
+            cin >> n;
+            bt.insert1(n);
+            break;
+        case 2:
+            cout << "Enter element to delete: ";
+            cin >> key;
+            bt.Delete(key);
+            break;
+        case 3:
+            cout << endl;
+            bt.pretrav();
+            break;
+        case 4:
+            cout << endl;
+            bt.intrav();
+            break;
+        case 5:
+            cout << endl;
+            bt.posttrav();
+            break;
+        case 6:
+            exit(0);
+        }
+    }
+    return 0;
 }
