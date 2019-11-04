d62ff554dbb90c103475ae473007827d2d3ea9c6
drishtichouhan <45739592+drishtichouhan@users.noreply.github.com>
2019-11-04 13:37:19 +0530

    Update stack.cpp

--- lab_assignment_slot1/stack.cpp
+++ lab_assignment_slot1/stack.cpp
@@ -1,63 +1,119 @@
-#include <bits/stdc++.h>
-using namespace std; 
-int stack[100], n=100, top=-1;
-void push(int val) {
-   if(top>=n-1)
-      cout<<"Stack Overflow"<<endl; 
-   else {
-      top++;
-      stack[top]=val;
-   }
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+/*
+ * Node Declaration
+ */
+struct node
+{
+    int info;
+    struct node *link;
+}*top;
+
+/*
+ * Class Declaration
+ */
+class stack_list
+{
+    public:
+        node *push(node *, int);
+        node *pop(node *);
+        void traverse(node *);
+        stack_list()
+        {
+            top = NULL;
+        }
+};
+
+/*
+ * Main: Contains Menu
+ */
+int main()
+{
+    int choice, item;
+    stack_list sl;
+    while (1)
+    {
+        cout<<"\n-------------"<<endl;
+        cout<<"Operations on Stack"<<endl;
+        cout<<"\n-------------"<<endl;
+        cout<<"1.Push Element into the Stack"<<endl;
+        cout<<"2.Pop Element from the Stack"<<endl;
+        cout<<"3.Traverse the Stack"<<endl;
+        cout<<"4.Quit"<<endl;
+        cout<<"Enter your Choice: ";
+        cin>>choice;
+        switch(choice)
+        {
+        case 1:
+            cout<<"Enter value to be pushed into the stack: ";
+            cin>>item;
+            top = sl.push(top, item);
+            break;
+        case 2:
+            top = sl.pop(top);
+            break;
+        case 3:
+            sl.traverse(top);
+            break;
+        case 4:
+            exit(1);
+            break;
+        default:
+            cout<<"Wrong Choice"<<endl;
+        }
+    }
+    return 0;
 }
-void pop() {
-   if(top<=-1)
-      cout<<"Stack Underflow"<<endl;
-   else {
-      cout<<"The popped element is "<< stack[top] <<endl;
-      top--;
-   }
+
+/*
+ * Push Element into the Stack
+ */
+node *stack_list::push(node *top, int item)
+{
+    node *tmp;
+    tmp = new (struct node);
+    tmp->info = item;
+    tmp->link = top;
+    top = tmp;
+    return top;
 }
-void display() {
-   if(top>=0) {
-      cout<<"Stack elements are:";
-      for(int i=top; i>=0; i--)
-         cout<<stack[i]<<" ";
-         cout<<endl;
-   } else
-      cout<<"Stack is empty";
+
+/*
+ * Pop Element from the Stack
+ */
+node *stack_list::pop(node *top)
+{
+    node *tmp;
+    if (top == NULL)
+        cout<<"Stack is Empty"<<endl;
+    else
+    {
+        tmp = top;
+        cout<<"Element Popped: "<<tmp->info<<endl;
+        top = top->link;
+        delete(tmp);
+    }
+    return top;
 }
-int main() {
-   int ch, val; 
-   cout<<"1) Push in stack"<<endl;
-   cout<<"2) Pop from stack"<<endl;
-   cout<<"3) Display stack"<<endl;
-   cout<<"4) Exit"<<endl;
-   do {
-      cout<<"Enter choice: "<<endl;
-      cin>>ch;
-      switch(ch) {
-         case 1: {   
-            cout<<"Enter value to be pushed:"<<endl;
-            cin>>val;
-            push(val);
-            break;
-         }
-         case 2: {
-            pop();
-            break;
-         }
-         case 3: {
-            display();
-            break;
-         }
-         case 4: {
-            cout<<"Exit"<<endl;
-            break;
-         }
-         default: {
-            cout<<"Invalid Choice"<<endl;
-         }
-      }
-   }while(ch!=4); 
-      return 0;
+
+/*
+ * Traversing the Stack
+ */
+void stack_list::traverse(node *top)
+{
+    node *ptr;
+    ptr = top;
+    if (top == NULL)
+        cout<<"Stack is empty"<<endl;
+    else
+    {
+        cout<<"Stack elements :"<<endl;
+        while (ptr != NULL)
+        {
+            cout<<ptr->info<<endl;
+            ptr = ptr->link;
+        }
+    }
 }
