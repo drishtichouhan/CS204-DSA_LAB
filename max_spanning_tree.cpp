d324a5dc00d1eaa4d1751b808055c0a3541fef29
drishtichouhan <45739592+drishtichouhan@users.noreply.github.com>
2019-11-06 08:00:03 +0530

    Update max_spanning_tree.cpp

--- max_spanning_tree.cpp
+++ max_spanning_tree.cpp
@@ -1,63 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef pair<int ,int> my_pair;
 
-struct Graph
+int arr[10000];
+int str[10000];
+
+int find(int a)
 {
-  int V,E;
-  vector< pair < int, my_pair > edges;
-  Graph(int V , int E)
-  {
-    this->V = V;
-    this->E = E;
-  }
-  void add_edge(int u , int v , int w)
-  {
-    edges.push_back({ w, {u , v } });
-  }
-  int kruskalMST();
+	if (a == arr[a])
+		return a;
 
-};
-struct DisjointSets
+	return arr[a] = find(arr[a]);
+}
+
+bool join(int a, int b)
 {
-  int *parent ,*rnk;
-  int n;
-  DisjointSets(int n)
-  {
-    this->n = n;
-    parent = new int[n+1];
-    for(int i=0;i<=n;i++)
-    {
-      rnk[i]=0;
-      parent[i]=i;
+	a = find(a);
+	b = find(b);
 
-    }
-  }
-  int find(int u)
-  {
-    if(u!=parent[u])parent[u]=find(parent[u]);
-    return parent[u];
-  }
-  void merge(int x, int y)
-  {
-    x=find(x) , y= find(y);
-    if(rnl[x]>rnk[y])parent[x]=y;
-    else parent[x] = y;
-    if(rnk[x]==rnk[y])rnk[y]++;
-  }
-};
+	if (a==b)	return false;
 
-int Graph :: kruskalMST()
+	if (str[a] < str[b])
+		swap (a,b);
+
+	ste[b] = a;
+	if (str[a] == str[b])
+		str[a]++;
+
+	return true;
+}
+
+int main()
 {
-  int mst_wt = 0;
-  sort(edges.begin(),edges.end());
-  DisjointSets ds(V);
-  vector< pair < int , my_pair> > :: iterator it;
-  for(it.edges.begin();it!=edges.end();it++)
-  {
-    int u=it->second.first;
-    int v=it->second.second;
+	long long weight = 1;
 
-    int set_u = ds.find(u)
-  }
+	int n,m;
+	cin >> n >> m;
+
+	vector <pair < long long, pair<int,int> > > v;
+
+	for (int i=0; i<n; i++)
+	{
+		arr[i] = i;
+		str[i] = 1;
+	}
+
+	for (int i=0; i<m; i++)
+	{
+		long long k;
+		int a,b;
+		cin >> a >> b >> k;
+
+		v.push_back(make_pair(k, make_pair(a,b)));
+	}
+
+	sort(v.rbegin(), v.rend());
+
+	for (auto it: v)
+	{
+		long long k;
+		int a,b;
+
+		k = it.first;
+		a = it.second.first;
+		b = it.second.second;
+
+		if (join(a,b))
+			weight = (weight * k) % 1000000007;
+	}
+
+	cout << weight << "\n";
+	return 0;
 }
